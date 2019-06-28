f991e16a72f58e234687e92fc1d36255949fb73d
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2019-06-27 17:26:25 -0700

    exercise route fixed

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -3,21 +3,20 @@ const router = express.Router();
 let Exercise = require("../models/exercise");
 
 router.get("/", (req, res) => {
-    Exercise.find()
-    .then(users => res.status(200).json(users))
+  Exercise.find()
+    .then(exercises => res.status(200).json(exercises))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
 router.post("/add", (req, res) => {
-  const { username, description, duration } = req.body;
+  const { username, description } = req.body;
+  const duration = Number(req.body.duration);
   const date = Date.parse(req.body.date);
 
   const newExercise = new Exercise({ username, description, duration, date });
 
   newExercise.save()
-    .then(() => {
-      res.status(200).json("New User added");
-    })
+    .then(() => res.status(200).json("New User added"))
     .catch(err => res.status(400).json("Error: " + err));
 });
 

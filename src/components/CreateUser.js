7b42845ea994928317cfc5e89951138b3c11690f
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2019-06-28 13:00:52 -0700

    create user form created

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,10 +1,56 @@
 import React, { Component } from "react";
 
 class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ""
+    };
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username
+    };
+
+    console.log(user);
+
+    this.setState({
+      username: ""
+    });
+  };
+
   render() {
     return (
       <div>
-        <p>Create User</p>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              name="username"
+              value={this.state.username}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }

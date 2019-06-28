a4541ce875ec6dd129e79cdcadaa7763016fa6ba
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2019-06-28 11:01:08 -0700

    components created

---
+++ src/components/EditExercise.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EditExercise extends Component {
+  render() {
+    return (
+      <div>
+        <p>Edit Exercise</p>
+      </div>
+    );
+  }
+}
+
+export default EditExercise;
---
+++ src/components/ExerciseList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ExerciseList extends Component {
+  render() {
+    return (
+      <div>
+        <p> Exercise List </p>
+      </div>
+    );
+  }
+}
+
+export default ExerciseList;
